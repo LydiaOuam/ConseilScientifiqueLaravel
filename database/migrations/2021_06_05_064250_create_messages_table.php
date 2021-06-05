e7bcd9f34998d99112f6cb06da76c35e780ada2d
LydiaOuam <lydiaouamrane@gmail.com>
2021-06-05 07:50:05 +0100

    Messages table

---
+++ database/migrations/2021_06_05_064250_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('expéditeur');
+            $table->string('destinataire');
+            $table->string('titre');
+            $table->text('contenu');
+            $table->boolean('vu')->default(0); // le message n'est pas vu
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}

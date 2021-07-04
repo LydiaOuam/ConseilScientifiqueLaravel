26647c34669bc6f4901dd78e65a12c3045bf5d69
LydiaOuam <lydiaouamrane@gmail.com>
2021-07-04 22:27:10 +0100

    ....

---
+++ database/migrations/2021_07_04_221657_create_presences_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presences', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idUser'); 
+            $table->foreign('idUser')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->unsignedBigInteger('idSessionCSD'); 
+            $table->foreign('idSessionCSD')
+                  ->references('idSessionCSD')->on('session_c_s_d_s')
+                  ->onDelete('cascade');
+            $table->enum('etat',['Présent','Absence justifiée','Absent']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presences');
+    }
+}
